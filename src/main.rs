863da30bc45d9cddc4b5e5a67da38868058a85f9
mercy <90040892+paracure@users.noreply.github.com>
2023-10-17 14:44:34 +0530

    end me 2

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 struct CliArgs {
-    pub string: Option<Vec<u8>>,
+    pub string: Option<String>,
     #[clap(short = 'V', long)]
     pub version: bool,
 }
 
 const VERSION_STRING: &'static str = env!("CARGO_PKG_VERSION");
-const BUFFER_SIZE: usize = 8192;
+// const BUFFER_SIZE: usize = 8192;
+const YES: &str = "y\n";
 
 fn main() -> anyhow::Result<()> {
     let mut stdout = std::io::stdout();
@@ -21,14 +22,13 @@ fn main() -> anyhow::Result<()> {
     // let mut writer = BufWriter::with_capacity(BUFFER_SIZE, stdout);
     match args.string {
         None => {
-            const YES: &str = "y\n";
             loop {
-                stdout.write(YES.as_ref())?;
+                write!(stdout, "{YES}")?;
             }
         }
         Some(vector) => {
             loop {
-                stdout.write(&vector)?;
+                write!(stdout, "{}", vector)?;
             }
         }
     }
