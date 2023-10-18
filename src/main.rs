789e2c864021876686c39652cefe95ac21229660
mercy <90040892+paracure@users.noreply.github.com>
2023-10-18 18:21:49 +0530

    small rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,61 @@
 use std::io::{BufWriter, Write};
-use clap::Parser;
+use clap::{CommandFactory, Parser};
+
 
 #[derive(Debug, Parser)]
+#[clap(disable_help_flag = true)]
 struct CliArgs {
+    /// string to print (if none is passed print 'y')
     pub string: Option<String>,
+    /// output version information and exit
     #[clap(short = 'V', long)]
     pub version: bool,
+    /// display this help and exit
+    #[clap(short, long)]
+    help: bool,
 }
 
 const VERSION_STRING: &'static str = env!("CARGO_PKG_VERSION");
-const BUFFER_SIZE: usize = 4 * 1024 * 1024;
+const BUFFER_SIZE: usize = 4096;
 
 fn main() -> anyhow::Result<()> {
     let mut stdout = std::io::stdout().lock();
     let args = CliArgs::parse();
+    // this if statement only exists so I can have a custom help message
+    if args.help {
+        let mut cli = CliArgs::command();
+        cli.build();
+        cli.print_help()?;
+        std::process::exit(0);
+    }
     if args.version {
         writeln!(stdout, "{VERSION_STRING}")?;
         return anyhow::Ok(());
     }
-    let mut buffer = vec![0u8; BUFFER_SIZE];
-    let mut writer = BufWriter::with_capacity(BUFFER_SIZE, stdout);
-    match args.string {
+    let size;
+    let mut writer = BufWriter::new(stdout);
+    let content = match args.string {
         None => {
-            write!(buffer, "y\n")?;
+            format!("y\n")
+            // write!(buffer, "y\n")?;
+            // buffer.flush()?;
         }
         Some(string) => {
-            write!(buffer, "{string}\n")?;
+            format!("{string}\n")
+            // write!(buffer, "{string}\n")?;
+            // buffer.flush()?;
         }
     };
-    while writer.write_all(&buffer).is_ok() {}
-    writer.flush()?;
-    anyhow::Ok(())
+    if content.len() < BUFFER_SIZE {
+        size = BUFFER_SIZE;
+    } else {
+        size = content.len().next_power_of_two();
+    }
+    let mut buffer = vec![0u8; size];
+    buffer.flush()?;
+    write!(buffer, "{content}")?;
+    loop {
+        writer.write_all(&buffer)?;
+        writer.flush()?;
+    }
 }
