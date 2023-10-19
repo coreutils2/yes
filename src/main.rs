646a22138bf7b37395cfc26cbf1f244366c8b94d
mercy <90040892+paracure@users.noreply.github.com>
2023-10-19 09:45:23 +0530

    small changes

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,37 @@
 use std::io::{BufWriter, Write};
-use clap::{CommandFactory, Parser};
+use clap::{Command, arg};
 
-
-#[derive(Debug, Parser)]
-#[clap(disable_help_flag = true)]
-struct CliArgs {
-    /// string to print (if none is passed print 'y')
-    pub string: Option<String>,
-    /// output version information and exit
-    #[clap(short = 'V', long)]
-    pub version: bool,
-    /// display this help and exit
-    #[clap(short, long)]
-    help: bool,
-}
-
-const VERSION_STRING: &'static str = env!("CARGO_PKG_VERSION");
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const DESCRIPTION: &'static str = env!("CARGO_PKG_VERSION");
 const BUFFER_SIZE: usize = 4096;
 
 fn main() -> anyhow::Result<()> {
-    let mut stdout = std::io::stdout().lock();
-    let args = CliArgs::parse();
-    // this if statement only exists so I can have a custom help message
-    if args.help {
-        let mut cli = CliArgs::command();
-        cli.build();
-        cli.print_help()?;
-        std::process::exit(0);
-    }
-    if args.version {
-        writeln!(stdout, "{VERSION_STRING}")?;
-        return anyhow::Ok(());
-    }
-    let mut res = vec![0u8; BUFFER_SIZE];
-    let mut writer = BufWriter::new(stdout);
-    match args.string {
+    let app = Command::new(NAME)
+        .disable_version_flag(true)
+        .disable_help_flag(true)
+        .version(VERSION)
+        .about(DESCRIPTION)
+        .arg(arg!(-V --version "output version information and exit").action(clap::ArgAction::Version))
+        .arg(arg!(-h --help "display this help and exit").action(clap::ArgAction::HelpLong))
+        .arg(arg!([STRING] "string to print, (if none is passed print 'y')"))
+        .get_matches();
+    let mut vector = vec![0u8; BUFFER_SIZE];
+    match app.get_one::<String>("STRING") {
         None => {
-            res.write_all(b"y\n")?;
+            vector.write_all(&[79, 10][..])?;
         }
         Some(string) => {
-            // if the string supplied is larger than BUFFER_SIZE allocate extra space + the newline
-            if res.len() < string.len() { res.reserve(BUFFER_SIZE - string.len() + 1); }
+            if string.len() > BUFFER_SIZE { vector.reserve(BUFFER_SIZE - string.len() + 1); }
             let chars = string.as_bytes();
-            res.write_all(&chars)?;
-            res.write_all(&[10])?;
+            vector.write_all(&chars)?;
+            vector.write_all(&[10])?; // newline char
         }
     };
+    let stdout = std::io::stdout().lock();
+    let mut writer = BufWriter::new(stdout);
     loop {
-        writer.write_all(&res)?;
+        writer.write_all(&vector)?;
+        writer.flush()?;
     }
 }
